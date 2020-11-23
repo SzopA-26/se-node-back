77aa6d34be3e74be7ee5eae918800f0e5ae82538
SzopA-26 <patiphat.kh@ku.th>
2020-11-23 13:52:21 +0700

    add route user

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
-// const session = require('express-session');
-// const bodyParser = require('body-parser');
-// const path = require('path');
-const db = require('./db-connection')
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const path = require('path');
+const db = require('./db-connection');
 const app = express();
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 9090
 
@@ -12,8 +14,6 @@ const PORT = process.env.PORT || 9090
 // 	resave: true,
 // 	saveUninitialized: true
 // }));
-// app.use(bodyParser.urlencoded({extended : true}));
-// app.use(bodyParser.json());
 
 // app.post('/auth', function(req, res) {
 // 	var email = req.body.email;
@@ -37,19 +37,75 @@ const PORT = process.env.PORT || 9090
 // });
 
 app.get('/api', function (req, res) {
-
     res.send('hello world');
 })
 
-app.get('/api/users', (req, res) => {
-
-    db.query("SELECT * FROM USERS", 
-    (err, result) => {
+// app.get('/api/users', (req, res) => {
+//     let sql = "SELECT * FROM USERS"
+//     db.query(sql, (err, result) => {
+//         if (err) throw err;
+//         console.log(sql);
+//         res.send(result)
+//     });
+// })
+app.route('/api/users')
+    .get((req, res) => {
+        let sql = "SELECT * FROM users"
+        db.query(sql, (err, result) => {
+            if (err) throw err;
+            console.log(sql);
+            res.send(result)
+        });
+    })
+    .post((req, res) => {
+        let sql = "INSERT INTO users (title, first_name, last_name, email, password, gender, role) " +
+                    "VALUES (?, ?, ?, ?, ?, ?, ?)"
+        db.query(sql, [req.body.title, req.body.first_name, req.body.last_name, req.body.email, req.body.password, req.body.gender, req.body.role], (err, result) => {
+            if (err) throw err;
+            console.log(sql);
+            res.send("Created user " + req.body.email);
+        })
+    })
+    .put((req, res) => {
+        let sql = "UPDATE users SET room_id = ?, " + 
+                    "title = ?, first_name = ?, last_name = ?, email = ?, birth_date = ?, gender = ?, citizen_id = ?, " + 
+                    "address = ?, phone_number_1 = ?, phone_number_2 = ?, money = ?, invited = ?, img = ?, checkIn_at = ? " +
+                    "WHERE id = ?"
+        db.query(sql, 
+                [req.body.room_id, req.body.title, req.body.first_name, req.body.last_name, req.body.email, 
+                req.body.birth_date, req.body.gender, req.body.citizen_id, req.body.address, req.body.phone_number_1, 
+                req.body.phone_number_2, req.body.money, req.body.invited, req.body.img, req.body.checkIn_at, req.body.id], 
+                (err, result) => {
+                    if (err) throw err;
+                    console.log(sql);
+                    res.send("Updated user id" + req.body.id + " " + req.body.email);
+                })
+    })
+app.get('/api/user/:id', (req, res) => {
+    let sql = "SELECT * FROM users WHERE id = ?"
+    db.query(sql, [req.params.id], (err, result) => {
         if (err) throw err;
         console.log(sql);
         res.send(result)
-    });
+    })
 })
+app.get('/api/room/:id', (req, res) => {
+    let sql = "SELECT * FROM rooms WHERE id = ?"
+    db.query(sql, [req.params.id], (err, result) => {
+        if (err) throw err;
+        console.log(sql);
+        res.send(result)
+    })
+})
+app.get('/api/room_image/:id/', (req, res) => {
+    let sql = "SELECT * FROM room_images WHERE room_id = ?"
+    db.query(sql, [req.params.id], (err, result) => {
+        if (err) throw err;
+        console.log(sql);
+        res.send(result)
+    })
+})
+
   
 app.listen(PORT, () => {
     console.log("Start server at PORT", PORT);
